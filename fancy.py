2cc45e23be6c324de303625d5ff568186ba7b08b
Heinrich Schnermann <github@schnermann.net>
2019-08-27 06:41:12 +0200

    Changed method to calculate fan speed to be linear. Added check for missing/misspelled values.

--- fancy.py
+++ fancy.py
@@ -12,14 +12,14 @@ import configparser
 LOGLEVEL =      1  # 0: silent, 1: important msgs, 2: verbose, 3: very verbose
 FAN_GPIO =      14 # Pin 8, Pin 4 = +5V, Pin 6 = Gnd
 WAIT_TIME =     10 # s
+MIN_TEMP =      40 # °C
+MAX_TEMP =      50 # °C
 
+THRESHOLD =     5  # °C
 PWM_FREQ =      50 # Hz
 TEMPFILE_STR =  "/sys/class/thermal/thermal_zone0/temp"
 LOGFILE_STR =   "/var/log/fancy.log"
 CONF_FILE_STR = "/etc/fancy.conf"
-
-tempSteps =     [40, 42, 45, 50, 55, 60]   # °C
-speedSteps =  [0, 20, 40, 70, 80, 90, 100] # %
 
 def log(verboselevel, message):
     if verboselevel <= LOGLEVEL:
@@ -31,12 +31,45 @@ def endProg(error = 0):
     GPIO.cleanup()
     sys.exit(error)
 
-conf = configparser.ConfigParser()
-conf.read(CONF_FILE_STR)
-default = conf["Default"]
-LOGLEVEL=int(default["LOGLEVEL"].split('#')[0])
-WAIT_TIME=int(default["WAIT_TIME"].split('#')[0])
-FAN_GPIO=int(default["FAN_GPIO"].split('#')[0])
+parser = configparser.ConfigParser()
+parser.read(CONF_FILE_STR)
+default = parser["Default"]
+
+try:
+    LOGLEVEL=int(default["LOGLEVEL"].split('#')[0])
+except KeyError:
+    pass
+except ValueError:
+    pass
+
+try:
+    WAIT_TIME=int(default["WAIT_TIME"].split('#')[0])
+except KeyError:
+    pass
+except ValueError:
+    pass
+
+try:
+    FAN_GPIO=int(default["FAN_GPIO"].split('#')[0])
+except KeyError:
+    pass
+except ValueError:
+    pass
+
+try:
+    MIN_TEMP=int(default["MIN_TEMP"].split('#')[0])
+except KeyError:
+    pass
+except ValueError:
+    pass
+
+try:
+    MAX_TEMP=int(default["MAX_TEMP"].split('#')[0])
+except KeyError:
+    pass
+except ValueError:
+    pass
+
 
 signal.signal(signal.SIGTERM, endProg)
 GPIO.setmode(GPIO.BCM)
@@ -47,31 +80,35 @@ fan.start(0);
 
 log(1, "Starting up fancy.\n")
 log(1, "Log level: " + str(LOGLEVEL) + ", GPIO: " + str(FAN_GPIO) + ", Interval: " + str(WAIT_TIME) + "\n")
+log(1, "Min temperature: " + str(MIN_TEMP) + "°C, Max temperature: " + str(MAX_TEMP) + "°C\n")
 lastspeed = 0
 
-if (len(speedSteps) != len(tempSteps) + 1):
-    log(0, "There should be one more speedstep than tempstep\n")
+if (MIN_TEMP >= MAX_TEMP):
+    log(0, "Min temperature should be smaller than max temperature!\n")
     endProg(1)
 
 try:
     speed = 0
+    lastspeed = 1
     while(1):
         with open(TEMPFILE_STR, "r") as temperatureFile:
             temp = float(temperatureFile.read()) / 1000
 
-        if temp < tempSteps[0]:
-            speed = speedSteps[0]
-        elif temp >= tempSteps[len(tempSteps) -1]:
-            speed = speedSteps[len(speedSteps) -1]
-        for i in range(0, len(tempSteps) - 1):
-            if temp >= tempSteps[i] and temp < tempSteps[i + 1]:
-                speed = speedSteps[i + 1]
+        if temp <= MIN_TEMP:
+            speed = 0
+        elif temp >= MAX_TEMP:
+            speed = 100
+        else:
+            speed = int((100 / (MAX_TEMP - MIN_TEMP)) * (temp - MIN_TEMP))
 
-        if speed != lastspeed:
+        if (speed > lastspeed + THRESHOLD) or \
+           (speed < lastspeed - THRESHOLD) or \
+           (speed == 100 and lastspeed < 100) or \
+           (speed == 0 and lastspeed > 0):
             log(2, "Temp: " + str(temp) + "°C, Fanspeed: " + str(speed) + "%\n")
+            lastspeed = speed
         else:
             log(3, "Temp: " + str(temp) + "°C, Fanspeed: " + str(speed) + "%\n")
-        lastspeed = speed
 
         fan.ChangeDutyCycle(speed)
         time.sleep(WAIT_TIME)
