424e2dff6eb2225545a0969c4f1b24bcafaee803
Heinrich Schnermann <github@schnermann.net>
2019-10-25 15:46:02 +0200

    add rpm led

---
+++ rpm.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+import signal
+import sys
+
+DEBUG = False
+RED_LED_PIN =   23
+GREEN_LED_PIN = 24
+BLUE_LED_PIN =  25
+
+def endProg(error = 0):
+    print("rpm will end now, cleaning up.")
+    GPIO.cleanup()
+    sys.exit(error)
+
+signal.signal(signal.SIGTERM, endProg)
+
+t = time.time()
+dt = 0
+
+def falling_cb(channel):
+    global t
+    global dt
+    dt = time.time() - t
+    t = time.time()
+
+def setLeds(rpm):
+    mm = 2500
+    if rpm > mm * 2:
+        rpm = mm *2
+    if (rpm < mm):
+        red_led.ChangeDutyCycle(100)
+        green_led.ChangeDutyCycle(100 - rpm / mm * 100)
+        blue_led.ChangeDutyCycle(rpm / mm * 100)
+    else:
+        red_led.ChangeDutyCycle(100 - ((rpm - mm) / mm) * 100)
+        green_led.ChangeDutyCycle(((rpm - mm) / mm) * 100)
+        blue_led.ChangeDutyCycle(100)
+
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(15, GPIO.IN, GPIO.PUD_UP)
+    GPIO.add_event_detect(15, GPIO.FALLING, callback = falling_cb)
+    GPIO.setup(RED_LED_PIN, GPIO.OUT, initial = GPIO.HIGH)
+    GPIO.setup(GREEN_LED_PIN, GPIO.OUT, initial = GPIO.HIGH)
+    GPIO.setup(BLUE_LED_PIN, GPIO.OUT, initial = GPIO.HIGH)
+    red_led = GPIO.PWM(RED_LED_PIN, 100)
+    green_led = GPIO.PWM(GREEN_LED_PIN, 100)
+    blue_led = GPIO.PWM(BLUE_LED_PIN, 100)
+    red_led.start(0)
+    green_led.start(0)
+    blue_led.start(0)
+    while True:
+        time.sleep(1)
+        if time.time() - t > 1:
+            if DEBUG:
+                print("RPM = 0")
+            setLeds(0)
+        else:
+            freq = 1 / dt
+            rpm = freq * 60 / 2
+            if DEBUG:
+                print("RPM = {:.0f}".format(rpm))
+            setLeds(rpm)
+
+finally:
+    endProg(0)
+
+
